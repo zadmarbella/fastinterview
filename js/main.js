380b7fe50e8a5bd69745cf1ba08130210cde2905
Ezadkiel Villarico Marbella <zadkiel.m@live.com>
2013-06-01 04:10:05 -0400

    updated for reading in question json

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 var app = angular.module("app", [])
 
-
-function FastInterviewCtrl($scope, $timeout)
+function FastInterviewCtrl($scope, $http)
 {
-	$scope.question = "test question?";
+	$http.get('questions.json').success(function(data) {
+		console.info(data);
+		// random between 1 and n
+		$scope.question = data[1];
+	});
+		
 	$scope.counter = 15 + "s";
 }
-
 
 function KeyboardCtrl($scope)
 {
